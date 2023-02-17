864305bcc8531c1b01378cfbcff2b4af55c45f40
YeshayahuBennun <ybennun@yahoo.com>
2023-02-17 13:22:16 +0200

    LeetCode_88_Merge_Sorted_Arrays - Accepted

--- src/Solution.java
+++ src/Solution.java
@@ -20,24 +20,20 @@ public class Solution {
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        if(nums1[0]<=nums2[0]){
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m + n; j++) {
-                    if (nums2[i] < nums1[j]) {
-                        int aux = nums1[j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m + n; j++) {
+                if (nums2[i] < nums1[j]) {
+                    int aux = nums1[j];
+                    nums1[j] = nums2[i];
+                    nums1[j + 1] = aux;
+                    break;
+                } else {
+                    if (nums1[j] == 0) {
                         nums1[j] = nums2[i];
-                        nums1[j + 1] = aux;
                         break;
-                    } else {
-                        if (nums1[j] == 0) {
-                            nums1[j] = nums2[i];
-                            break;
-                        }
                     }
                 }
             }
-        }else{
-            //TODO
         }
         System.out.println(Arrays.toString(nums1));
     }
---
+++ src/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int[] nums2 = {2, 5, 6};
+        int m = 3;
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = 0;
+        for (int i = m; i < nums1.length; i++) {
+            nums1[i]=nums2[j];
+            j++;
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
